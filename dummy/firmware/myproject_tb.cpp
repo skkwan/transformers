a5fb253c79d6246ac54babb47d259c851ee1ce89
Stephanie Kwan <skwan@princeton.edu>
2023-07-13 08:25:16 -0500

    add 100 input values to test bench and change test bench to use in/out files

--- dummy/firmware/myproject_tb.cpp
+++ dummy/firmware/myproject_tb.cpp
@@ -3,10 +3,16 @@
 #include <iostream>
 #include <fstream>
 
+/*
+ * Exact implementation of GELU.
+ */
 float gelu_function_float(float in) {
     return in * 0.5 * (1 + std::erf(in / std::sqrt(2)));
 }
 
+/*
+ * Write a GELU table to a file gelu_table_temp.h. The table should be pasted into the GELU function.
+ */
 template <class data_T, typename CONFIG_T> void init_gelu_table(data_T table_out[CONFIG_T::table_size]) {
 
     // To-do: Try to have the GELU table be a header file generated in the tb,
@@ -28,45 +34,84 @@ template <class data_T, typename CONFIG_T> void init_gelu_table(data_T table_out
     myfile << "};";
 }
 
+/*
+ * 
+ */
+template<class data_T, unsigned int table_size>
+void fillArrayFromTable(std::string filename, data_T (&table)[table_size]) {
+    std::ifstream inFile;
+    std::cout << "Before opening input " << filename << std::endl;
+    inFile.open(filename, std::ifstream::in);
+    std::string line;
 
+    int nEntry = 0;
+
+    for (; std::getline(inFile, line); ) {
+        if (nEntry >= table_size) {
+            break;
+        }
+
+        std::cout << ".... read line " << line << std::endl;
+	    data_T data = (data_T) std::stof(line); // convert string to float, then cast to data_T
+        table[nEntry] = data;
+        nEntry++;
+    }
+}
+
+/*
+ * Test bench function.
+ */
 int main () {
 
-    // Get values for the table. Actually the init_gelu_table function writes everything to a .h file, but we have the option of
-    // manipulating this as a C++ object as well, e.g. as an input to the top-level function (to be decided).
-    table_t gelu_table_object[TABLE_SIZE];
+    // // ONCE ONLY: Get values for the table. Actually the init_gelu_table function writes everything to a .h file, but we have the option of
+    // // manipulating this as a C++ object as well, e.g. as an input to the top-level function (to be decided).
+    // table_t gelu_table_object[TABLE_SIZE];
 
-    init_gelu_table<table_t, gelu_config2>(gelu_table_object);
+    // // init_gelu_table<table_t, gelu_config2>(gelu_table_object);
 
     float target_max_rmsError = 0.2;
 
-    input_t myInput[N_INPUT_1_1] = {-3.0, -1.0, 0.0, 1.0, 3.0};
+    // Get array from input file
+    input_t myInput[N_INPUT_1_1];
+    fillArrayFromTable("input.txt", myInput);
+
     result_t myOutput[N_INPUT_1_1];
 
-    result_t goldenOutput[N_INPUT_1_1] = {-0.00404969409489031, -0.15865525393145707, 0.0, 0.8413447460685429, 2.99595030590511};
+    // Ideal output
+    result_t goldenOutput[N_INPUT_1_1];
+    fillArrayFromTable("idealOut.txt", goldenOutput);
 
     // Execute the function 
     myproject(myInput, myOutput);
 
     // Compare with the golden output: compute the root-mean-square error
-    int passTest;
     float numerator = 0;
     float rmsError = 99;
 
     for (int i = 0; i < N_INPUT_1_1; i++) {
-            // Print for our edification
-            float outAsFloat = myOutput[i].to_float();       
-            float goldenOutAsFloat = goldenOutput[i].to_float();  
-            std::cout << "For input " << myInput[i] << ", as float: output : " << outAsFloat << ", comparing with " << goldenOutAsFloat << std::endl;
-            // Increment the numerator
-            numerator += std::pow((goldenOutAsFloat - outAsFloat), 2);
+        // Print for our edification
+        float outAsFloat = myOutput[i].to_float();       
+        float goldenOutAsFloat = goldenOutput[i].to_float();  
+        std::cout << "For input " << myInput[i] << ", as float: output : " << outAsFloat << ", comparing with " << goldenOutAsFloat << std::endl;
+
+        // Increment the numerator for the RMS error
+        numerator += std::pow((goldenOutAsFloat - outAsFloat), 2);
+
+        // // Also check per-value that the percentage difference is less than 5%
+        // if (std::abs((outAsFloat - goldenOutAsFloat)/goldenOutAsFloat) > 0.05) {
+        //     std::cout << "Difference is greater than 5%, test failed" << std::endl;
+        //     return 1;
+        // }
     }
     
     rmsError = std::sqrt(numerator/N_INPUT_1_1);
 
     std::cout << "numerator " << numerator << ", rmsError " << rmsError << std::endl;
 
+    int passTest;
     if (rmsError < target_max_rmsError) passTest = 0;
     else                                passTest = 1;
 
     return passTest;
+
 }
